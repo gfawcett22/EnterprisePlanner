c0e1e4140a2c42e2a766fe516f4ff3c09a2950eb
Garron Fawcett <gfawcett22@gmail.com>
2017-06-10 01:24:57 -0400

    Created api gateway project and added image on purposed architecture

---
+++ API_Gateway/Contexts/APIGatewayContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Gateway.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Gateway.Contexts
+{
+    public class ApiGatewayContext : DbContext
+    {
+        public ApiGatewayContext(DbContextOptions<ApiGatewayContext> options) : base(options) { }
+        public DbSet<Endpoint> Endpoints { get; set; }
+    }
+
+}
---
+++ API_Gateway/Entities/Endpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gateway.Entities
+{
+    public class Endpoint
+    {
+        public int Id { get; set; }
+        public string IP { get; set; }
+        public string DNSName { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
